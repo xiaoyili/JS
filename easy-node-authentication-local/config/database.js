427f1230c9cebfb4f85596310bc68307c42d0065
xiaoyili <us.ericlee@gmail.com>
2014-02-24 16:09:52 -0500

    1
    1

---
+++ easy-node-authentication-local/config/database.js
@@ -0,0 +1,10 @@
+// config/database.js
+module.exports = {
+	
+	// mongodb://localhost/testAPP
+	
+	'url' : 'mongodb://maocool:861219@novus.modulusmongo.net:27017/uzOqos3y' 
+	
+	// mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+
+};
